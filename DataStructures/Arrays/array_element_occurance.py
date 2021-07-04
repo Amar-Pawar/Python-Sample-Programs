e40518e7886dc4014874889825b25d983d0604f3
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-07-04 22:09:02 +0530

    [Amar Pawar] Add:Function to print occurance of specific element from array

---
+++ DataStructures/Arrays/array_element_occurance.py
@@ -0,0 +1,29 @@
+'''
+/**********************************************************************************
+@Author: Amar Pawar
+@Date: 2021-07-04
+@Last Modified by: Amar Pawar
+@Last Modified time: 2021-07-04
+@Title : To get number of occurances of specific elements in array
+/**********************************************************************************
+'''
+from collections import Counter
+import logging
+from logging_handler import logger
+
+
+class Array():
+    def count_elements(self):
+        """
+        Description:
+            This function prints occurances of specific elements in array by using counter() function
+            imported from python collections.
+        """
+        array_list = ["1", "5", "5", "5", "1"]
+        # using counter to get occurances
+        occurrences = Counter(array_list)
+        logger.info(occurrences)
+        logger.info(occurrences["5"])
+
+counter = Array()
+counter.count_elements()
