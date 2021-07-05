ddb73bbc25a02047d2cbdb3ec9f905e997c3960f
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-07-05 17:11:27 +0530

    [Amar Pawar] Add:Function to print list elements as key of nested dictionary

---
+++ DataStructures/Dictionary/list_to_nested_dict.py
@@ -0,0 +1,25 @@
+'''
+/**********************************************************************************
+@Author: Amar Pawar
+@Date: 2021-07-05
+@Last Modified by: Amar Pawar
+@Last Modified time: 2021-07-05
+@Title : Print list as key with nested dictionary.
+/**********************************************************************************
+'''
+import logging
+from logging_handler import logger
+
+def list_to_dict():
+    """
+    Description:
+        This function prints list as keys with nested dictionaries. 
+    """
+    name_list = ["Amar", "sagar", "sanket", "swaraj"]
+    dict = current = {}
+    for name in name_list:
+        current[name] = {}
+        current = current[name]
+    logger.info(f"dictionary with nested keys: {dict}")
+
+list_to_dict()
