80da9d043e5edf16e89bad6f998564e8f361024f
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-07-06 09:39:48 +0530

    [Amar Pawar] Add:Function to remove duplicate elements from list

---
+++ DataStructures/Lists/remove_duplicate.py
@@ -0,0 +1,28 @@
+'''
+/**********************************************************************************
+@Author: Amar Pawar
+@Date: 2021-07-06
+@Last Modified by: Amar Pawar
+@Last Modified time: 2021-07-06
+@Title : Program to remove duplicate elements in list.
+/**********************************************************************************
+'''
+import logging
+from logging_handler import logger
+
+def Remove():
+    """
+    Description:
+        This function will remove duplicate elements in list.
+    """
+    list = [2, 4, 10, 20, 5, 2, 20, 4]
+    final_list = []
+    try:
+        for num in list:
+            if num not in final_list:
+                final_list.append(num)
+        print(final_list)
+    except Exception as e:
+        logger.info(f"Error!! {e}")
+        
+Remove()
